c0d70a7aeabd3fd69d66ea0bddd695e41a8e44d2
Ismail Karabulut <i.karabulut93@gmail.com>
2022-08-25 13:09:45 -0500

    WIP: trying to create POST

---
+++ src/TheEstimator/Controllers/PertController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace TheEstimator.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PertController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Create(Estimate estimate)
+    {
+        
+    }
+}
---
+++ test/TheEstimator.Tests/ControllersTests/PertControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheEstimator.Controllers;
+
+namespace TheEstimator.Tests.ControllersTests;
+
+public class PertControllerTests
+{
+    [Fact]
+    public void PertPost_Returns0_WhenModelStateIsAllZeros()
+    {
+        // Arrange
+        var controller = new PertController();
+
+        // Act
+        var result = controller.Create();
+
+        // Assert
+        var expectedResult = Assert.IsType<CreatedAtAction>(result);
+        Assert.IsType<SerializableError>(badRequestResult.Value);
+    }
+}
