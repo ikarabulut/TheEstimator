a5dbfe019657a378e9aa61f45fe1a9439aae0c8a
Ismail Karabulut <i.karabulut93@gmail.com>
2022-08-26 11:56:16 -0500

    adds PertCalculator exception for negative parameters

--- src/TheEstimator/EstimateCalculators/PertCalculator.cs
+++ src/TheEstimator/EstimateCalculators/PertCalculator.cs
@@ -4,7 +4,15 @@ public class PertCalculator : IEstimateCalculator
 {
     public int CalculateEstimate(int mostLikely, int optimistic, int pessimistic)
     {
+        ContainsNegativeParametersCheck(mostLikely, optimistic, pessimistic);
+        
         return (optimistic + (4 * mostLikely) + pessimistic) / 6;
+    }
+
+    private void ContainsNegativeParametersCheck(int mostLikely, int optimistic, int pessimistic)
+    {
+        if (mostLikely < 0 || optimistic < 0 || pessimistic < 0)
+            throw new ArgumentException("Values cannot be negative");
     }
 
 }
--- test/TheEstimator.Tests/EstimateCalculatorsTests/PertCalculatorTests.cs
+++ test/TheEstimator.Tests/EstimateCalculatorsTests/PertCalculatorTests.cs
@@ -5,7 +5,7 @@ namespace TheEstimator.Tests.EstimateCalculatorsTests;
 public class PertCalculatorTests
 {
     [Fact]
-    public void CreatePert_AllZeros_ReturnsZero()
+    public void CreateEstimate_AllZeros_ReturnsZero()
     {
         PertCalculator calculator = new PertCalculator();
         const int expectedEstimate = 0;
@@ -13,5 +13,14 @@ public class PertCalculatorTests
         var returnedEstimate = calculator.CalculateEstimate(0, 0, 0);
 
         Assert.Equal(expectedEstimate, returnedEstimate);
+    }
+
+    [Fact]
+    public void CreateEstimate_WithNegativeParam_ThrowsIllegalArgumentException()
+    {
+        PertCalculator calculator = new PertCalculator();
+        var disallowedNegativeParam = -1;
+
+        Assert.Throws<ArgumentException>(() => calculator.CalculateEstimate(disallowedNegativeParam, 0, 0));
     }
 }
