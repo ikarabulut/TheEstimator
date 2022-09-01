e100110bf3f3b84c66c1eed403ee8b2fb756dad3
Ismail Karabulut <i.karabulut93@gmail.com>
2022-09-01 17:09:09 -0500

    formatting

--- src/TheEstimator/DataContext/EstimateContext.cs
+++ src/TheEstimator/DataContext/EstimateContext.cs
@@ -6,13 +6,13 @@ namespace TheEstimator.DataContext;
 public class EstimateContext : DbContext
 {
 
-    public EstimateContext(DbContextOptions<EstimateContext> options): base(options)
+    public EstimateContext(DbContextOptions<EstimateContext> options) : base(options)
     { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseSerialColumns();
     }
-    
+
     public DbSet<Estimate> Estimates { get; set; }
 }
