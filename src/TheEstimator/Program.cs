9a62030bce8d15ed4f5e165e5cc56ab57916490f
Ismail Karabulut <i.karabulut93@gmail.com>
2022-09-12 20:29:53 +0300

    removes unused startup settings

--- src/TheEstimator/Program.cs
+++ src/TheEstimator/Program.cs
@@ -31,19 +31,7 @@ builder.Services.AddCors(options =>
         });
 });
 
-// builder.WebHost.ConfigureKestrel(serverOptions =>
-// {
-//     serverOptions.ListenAnyIP(5001);
-// });
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
